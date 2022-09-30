3de4af5377471ab25a12ebc8d4ed02057c737b9e
AIA <mnbvcx0229@gmail.com>
2022-09-30 14:13:41 +0900

    Yong 09-30 14:13 게시판

---
+++ 입문/게시판.java
@@ -0,0 +1,80 @@
+package 입문;
+
+import java.util.*;
+public class 게시판 {
+    public static void main(String[] args) {
+        게시판 solution = new 게시판();
+        solution.solution();
+    }
+
+    public void solution(){
+        System.out.println(" ### 게시판 ### \n");
+        Scanner scanner = new Scanner(System.in);
+
+        
+        String[] board = new String[3];
+
+        while(true){
+            System.out.println("[메뉴] 0.종료 1.글쓰기 2. 글목록 3. 글삭제");
+            String menu = scanner.next();
+
+            switch(menu){
+                case "0" :
+                    System.out.println("종료합니다.");
+                    return;
+                case "1" :
+                    System.out.println("글쓰기");
+                    board = write(board, scanner);
+                    break;
+                case "2" :
+                    System.out.println("글쓰기");
+                    board = list(board);
+                    break;
+                case "3" : 
+                    System.out.println("글쓰기");
+                    board = delete(board);
+                    break;
+                default:
+                    System.out.println("없는 메뉴입니다. 다시 선택해주세요");
+            }
+        }
+    }
+
+    /**
+     * 글쓰기
+     */
+    public String[] write(String[] board, Scanner scanner){
+        int emptycount = 3;
+        int lastIndex = 0;
+
+        if(emptycount == 0){
+            System.out.println(" === 게시판 용량(3개)이 가득 찼습니다. === ");
+        }
+        else{
+            System.out.println(" === 글을 작성해 주세요 === ");
+            scanner.nextLine(); //스캐너 버그 때문에
+            String message = scanner.nextLine();
+            board[lastIndex] = message;
+            emptycount--;
+            lastIndex++;
+        }
+        
+        return board;
+    }
+
+    /**
+     * 글목록
+     */
+    public String[] list(String[] board){
+        System.out.println(board[0]);
+        return board;
+    }
+
+    /**
+     * 글삭제
+     */
+    public String[] delete(String[] board){
+        board[0] = "";
+        return board;
+    }
+}
