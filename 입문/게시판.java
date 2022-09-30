972d07064eb34169805e8dae3b7c43532a9d8f1e
AIA <mnbvcx0229@gmail.com>
2022-09-30 15:54:25 +0900

    Yong 09-30 15:54 게시판 최종완성

--- 입문/게시판.java
+++ 입문/게시판.java
@@ -11,8 +11,10 @@ public class 게시판 {
         System.out.println(" ### 게시판 ### \n");
         Scanner scanner = new Scanner(System.in);
 
-        
-        String[] board = new String[3];
+        String[] board = new String[4];
+        board = init(board);
+        int emptycount = 4;
+        int lastIndex = -1; //인덱스 0은 이미 존재하는 상황이므로
 
         while(true){
             System.out.println("[메뉴] 0.종료 1.글쓰기 2. 글목록 3. 글삭제");
@@ -24,15 +26,28 @@ public class 게시판 {
                     return;
                 case "1" :
                     System.out.println("글쓰기");
-                    board = write(board, scanner);
+                    if(emptycount <= 0){
+                        System.out.println(" === 게시판 용량(4개)이 가득 찼습니다. === ");
+                    }
+                    else{
+                        lastIndex++;
+                        board = write(board, emptycount, scanner);
+                        emptycount--;
+                    }
                     break;
                 case "2" :
-                    System.out.println("글쓰기");
+                    System.out.println("글목록");
                     board = list(board);
                     break;
                 case "3" : 
-                    System.out.println("글쓰기");
-                    board = delete(board);
+                if(emptycount == 3){
+                    System.out.println(" === 삭제할 글이 없습니다. === ");
+                }
+                else{
+                    board = delete(board, lastIndex, scanner);
+                    emptycount++;
+                    lastIndex--;
+                }
                     break;
                 default:
                     System.out.println("없는 메뉴입니다. 다시 선택해주세요");
@@ -41,23 +56,25 @@ public class 게시판 {
     }
 
     /**
+     * 배열 초기화
+     */
+    public String[] init(String[] board){
+        for(int i = 0; i < board.length; i++){
+            board[i] = "";
+        }
+
+        return board;
+    }
+
+    /**
      * 글쓰기
      */
-    public String[] write(String[] board, Scanner scanner){
-        int emptycount = 3;
-        int lastIndex = 0;
-
-        if(emptycount == 0){
-            System.out.println(" === 게시판 용량(3개)이 가득 찼습니다. === ");
-        }
-        else{
-            System.out.println(" === 글을 작성해 주세요 === ");
-            scanner.nextLine(); //스캐너 버그 때문에
-            String message = scanner.nextLine();
-            board[lastIndex] = message;
-            emptycount--;
-            lastIndex++;
-        }
+    public String[] write(String[] board, int lastIndex, Scanner scanner){
+        
+        System.out.println(" === 글을 작성해 주세요 === ");
+        scanner.nextLine(); //스캐너 버그 때문에
+        String message = scanner.nextLine();
+        board[lastIndex] = message;
         
         return board;
     }
@@ -66,15 +83,23 @@ public class 게시판 {
      * 글목록
      */
     public String[] list(String[] board){
-        System.out.println(board[0]);
+        for(int i = 0; i < board.length; i++){
+            System.out.println(String.format("[%d]번 게시글 : %s", i + 1, board[i]));
+        }
         return board;
     }
 
     /**
      * 글삭제
      */
-    public String[] delete(String[] board){
-        board[0] = "";
+    public String[] delete(String[] board, int lastIndex, Scanner scanner){
+        System.out.println("몇번 글을 삭제하시겠습니까? : ");
+        int deleteNumber = scanner.nextInt();
+        System.out.println("마지막 인덱스 : " + lastIndex);
+        
+        board[deleteNumber - 1] = board[lastIndex];
+        board[lastIndex] = "";
+
         return board;
-    }
+    }    
 }
